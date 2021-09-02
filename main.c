040dae3fee87511d46fc7bafb6ad6f709a0f5cfa
EA_Jeremy <1037063188@qq.com>
2021-09-02 14:31:25 +0800

    修改部分协议 增加部分表盘

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
  * @Author: Jeremy
  * @Date: 2021-08-09 14:23:55
  * @LastEditors: Jeremy
- * @LastEditTime: 2021-08-19 15:29:28
+ * @LastEditTime: 2021-09-02 14:26:04
  */
 /*
  * @LastEditTime: 2021-08-09 13:40:08
@@ -16,12 +16,10 @@
 #include "custom_dial_head.h"
 #include "cJSON/cJSON.h"
 
-uint8_t dial_data[4096];
-uint8_t obj_type[40]; 
-#define LV_HOR_RES_MAX 320
-#define LV_VER_RES_MAX 320
+uint8_t dial_data[8192];
+uint8_t obj_type[40];  
 
-uint8_t get_dial_type_size(uint8_t type)
+uint16_t get_dial_type_size(uint8_t type)
 {
 	if(type == dial_type_image)						return sizeof(custom_dial_image);
 	else if(type == dial_type_pointer) 		return sizeof(custom_dial_pointer);
@@ -29,174 +27,9 @@ uint8_t get_dial_type_size(uint8_t type)
 	else if(type == dial_type_chart_bar)  return sizeof(custom_dial_chart_bar);
 	else if(type == dial_type_text)			  return sizeof(custom_dial_text);
 	else if(type == dial_type_more_img) 	return sizeof(custom_dial_more_img);
+	else if(type == dial_type_gif) 	return sizeof(custom_dial_gif);
 	else return 0;
-}
-
-
-const custom_dial_more_img main9_watch_bg = {
-  .type= dial_type_image,
-  .x= 0,//左上角x坐标
-  .y= 0,//左上角y坐标
-  .img_addr[0] = main_30_001_320x320_png ,//图片nandflash相对地址
-  .img_addr[1] = main_30_002_320x320_png ,//图片nandflash相对地址
-  .img_addr[2] = main_30_003_320x320_png ,//图片nandflash相对地址
-  .img_addr[3] = main_30_004_320x320_png ,//图片nandflash相对地址
-  .img_addr[4] = main_30_005_320x320_png ,//图片nandflash相对地址
-  .img_addr[5] = main_30_006_320x320_png ,//图片nandflash相对地址
-  .img_addr[6] = main_30_007_320x320_png ,//图片nandflash相对地址
-  .img_addr[7] = main_30_008_320x320_png ,//图片nandflash相对地址
-  .img_addr[8] = main_30_009_320x320_png ,//图片nandflash相对地址
-  .img_addr[9] = main_30_010_320x320_png ,//图片nandflash相对地址
-  .img_addr[10]= main_30_011_320x320_png ,//图片nandflash相对地址
-  .img_addr[11]= main_30_012_320x320_png ,//图片nandflash相对地址
-  .img_addr[12]= main_30_013_320x320_png ,//图片nandflash相对地址
-  .img_addr[13]= main_30_014_320x320_png ,//图片nandflash相对地址
-  .img_addr[14]= main_30_015_320x320_png ,//图片nandflash相对地址
-  .img_addr[15]= main_30_016_320x320_png ,//图片nandflash相对地址  
-  .data_type = dial_data_img_gif,
-};
-const custom_dial_text 		 main9_watch_hour_high = {
-  .type = dial_type_text,
-  .data_type = dial_data_text_hour_high  ,
-  .x =  82 ,
-  .y =  22,        
-  .text_width = 35,
-  .text_rgb[0] = 0xff,
-  .text_rgb[1] = 0xff,
-  .text_rgb[2] = 0xff,
-  .text_align = LV_LABEL_ALIGN_LEFT,
-  .text_size = dial_data_60x60_ext,
-};
-const custom_dial_text 		 main9_watch_hour_low = {
-  .type = dial_type_text,
-  .data_type = dial_data_text_hour_low  ,
-  .x =  117 ,
-  .y =  22,        
-  .text_width = 35,
-  .text_rgb[0] = 0xff,
-  .text_rgb[1] = 0xff,
-  .text_rgb[2] = 0xff,
-  .text_align = LV_LABEL_ALIGN_LEFT,
-  .text_size = dial_data_60x60_ext,
-};
-const custom_dial_text 		 main9_watch_min_high = {
-  .type = dial_type_text,
-  .data_type = dial_data_text_min_high  ,
-  .x =  169 ,
-  .y =  22,        
-  .text_width = 35,
-  .text_rgb[0] = 0xff,
-  .text_rgb[1] = 0xff,
-  .text_rgb[2] = 0xff,
-  .text_align = LV_LABEL_ALIGN_LEFT,
-  .text_size = dial_data_60x60_ext,
-};
-const custom_dial_text 		 main9_watch_min_low = {
-  .type = dial_type_text,
-  .data_type = dial_data_text_min_low  ,
-  .x =  204 ,
-  .y =  22,        
-  .text_width = 35,
-  .text_rgb[0] = 0xff,
-  .text_rgb[1] = 0xff,
-  .text_rgb[2] = 0xff,
-  .text_align = LV_LABEL_ALIGN_LEFT,
-  .text_size = dial_data_60x60_ext,
-};
-const custom_dial_text 		 main9_watch_separator = {
-  .type = dial_type_text,
-  .data_type = dial_data_text_custom  ,
-  .x =  152 ,
-  .y =  22,        
-  .text_width = 35,
-  .text_rgb[0] = 0xff,
-  .text_rgb[1] = 0xff,
-  .text_rgb[2] = 0xff,
-  .text_align = LV_LABEL_ALIGN_LEFT,
-  .text_size = dial_data_60x60_ext,
-  .text_custom[0] = ':',
-  .text_custom[1] = '\n',
-};
-const custom_dial_text 		 main9_watch_month = {
-  .type = dial_type_text,
-  .data_type = dial_data_text_month  ,
-  .x =  119 ,
-  .y =  268,        
-  .text_width = 35,
-  .text_rgb[0] = 0xff,
-  .text_rgb[1] = 0xff,
-  .text_rgb[2] = 0xff,
-  .text_align = LV_LABEL_ALIGN_LEFT,
-  .text_size = dial_data_30x30_ext, 
-};
-const custom_dial_text 		 main9_watch_date = {
-  .type = dial_type_text,
-  .data_type = dial_data_text_day  ,
-  .x =  164 ,
-  .y =  268,        
-  .text_width = 35,
-  .text_rgb[0] = 0xff,
-  .text_rgb[1] = 0xff,
-  .text_rgb[2] = 0xff,
-  .text_align = LV_LABEL_ALIGN_LEFT,
-  .text_size = dial_data_30x30_ext, 
-};
-const custom_dial_text 		 main9_watch_separator2 = {
-  .type = dial_type_text,
-  .data_type = dial_data_text_custom  ,
-  .x =  154  ,
-  .y =  268,        
-  .text_width = 35,
-  .text_rgb[0] = 0xff,
-  .text_rgb[1] = 0xff,
-  .text_rgb[2] = 0xff,
-  .text_align = LV_LABEL_ALIGN_LEFT,
-  .text_size = dial_data_30x30_ext,
-  .text_custom[0] = '/',
-  .text_custom[1] = '\n',
-};
-
-void main9_data_init(void)
-{
-	uint8_t* memory = dial_data; 
-	obj_type[0] = dial_type_more_img;
-	obj_type[1] = dial_type_text;
-	obj_type[2] = dial_type_text;
-	obj_type[3] = dial_type_text;
-	obj_type[4] = dial_type_text;
-	obj_type[5] = dial_type_text;
-  obj_type[6] = dial_type_text;
-	obj_type[7] = dial_type_text;
-	obj_type[8] = dial_type_text;
-
-	memcpy(memory,&main9_watch_bg,get_dial_type_size(dial_type_more_img)); 
-	memory += get_dial_type_size(dial_type_more_img);
-
-	memcpy(memory,&main9_watch_hour_high,get_dial_type_size(dial_type_text)); 
-	memory += get_dial_type_size(dial_type_text);
-
-  memcpy(memory,&main9_watch_hour_low,get_dial_type_size(dial_type_text)); 
-	memory += get_dial_type_size(dial_type_text);
-
-  memcpy(memory,&main9_watch_min_high,get_dial_type_size(dial_type_text)); 
-	memory += get_dial_type_size(dial_type_text);
-
-  memcpy(memory,&main9_watch_min_low,get_dial_type_size(dial_type_text)); 
-	memory += get_dial_type_size(dial_type_text);
-
-  memcpy(memory,&main9_watch_separator,get_dial_type_size(dial_type_text)); 
-	memory += get_dial_type_size(dial_type_text);
-
-  memcpy(memory,&main9_watch_month,get_dial_type_size(dial_type_text)); 
-	memory += get_dial_type_size(dial_type_text);
-
-  memcpy(memory,&main9_watch_date,get_dial_type_size(dial_type_text)); 
-	memory += get_dial_type_size(dial_type_text);
-
-  memcpy(memory,&main9_watch_separator2,get_dial_type_size(dial_type_text)); 
-	memory += get_dial_type_size(dial_type_text);
-}
-
+} 
 
  
 /* 返回ch字符在sign数组中的序号 */
@@ -418,6 +251,32 @@ int main()
       memcpy(memory,&dat,get_dial_type_size(dial_type_more_img)); 
       memory += get_dial_type_size(dial_type_more_img);
     }
+    else if(read_type->valueint == dial_type_gif)
+    {
+      cJSON* cjson_type           =  cJSON_GetObjectItem(obj,"type");
+      cJSON* cjson_data_type      =  cJSON_GetObjectItem(obj,"data_type");
+      cJSON* cjson_x              =  cJSON_GetObjectItem(obj,"x");
+      cJSON* cjson_y              =  cJSON_GetObjectItem(obj,"y");
+      cJSON* cjson_img_addr       =  cJSON_GetObjectItem(obj,"img_addr");
+      custom_dial_more_img dat;
+      dat.type      = cjson_type->valueint;
+      dat.data_type = cjson_data_type->valueint;
+      dat.x         = cjson_x->valueint;
+      dat.y         = cjson_y->valueint; 
+      for(i = 0;i<cJSON_GetArraySize(cjson_img_addr);i++)
+      {
+        cJSON* r =cJSON_GetArrayItem(cjson_img_addr,i);
+        dat.img_addr[i] = get16CharToInt(r->valuestring);
+      }
+      printf("%d %d %d %d ",dat.type,dat.data_type ,dat.x,dat.y);
+      for(i = 0;i<cJSON_GetArraySize(cjson_img_addr);i++)
+        printf("%d ",dat.img_addr[i]);
+      printf("\n");
+
+      obj_type[obj_num++] = dat.type;
+      memcpy(memory,&dat,get_dial_type_size(dial_type_gif)); 
+      memory += get_dial_type_size(dial_type_gif);
+    }
   }
 
 
@@ -428,19 +287,16 @@ int main()
   uint8_t crc8[8] = {0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff};
   uint8_t head_bin4[4] = { 0x00,0x00,0xa0,0x06};
 
-
-  // main9_data_init();
   printf("\nhello world\n");
 
   FILE *fp = NULL; 
   
 
 
-  char file_name[] = "001001_U0.";
+  char file_name[] = "dial_bin//240/001240_U0.";
   strcat(file_name,cjson_dial_id->valuestring);
-  uint32_t dial_id = atoi(cjson_dial_id->valuestring);
   strcat(file_name,".bin");
-  printf("dial_ia = %d\r\n",dial_id);
+  uint32_t dial_id[32] = {0};
 
   uint32_t little_img_addr = get16CharToInt(cjson_dial_little_addr->valuestring);
   printf("little_img_addr = 0x%x\n",little_img_addr);
@@ -459,7 +315,7 @@ int main()
   while((	c=fgetc(f_rb))!=EOF) { putc(c, fp);}	
   fclose(f_rb); 
 
-  // fwrite(&crc8,sizeof(crc8),1,fp);     //crc 放在尾部  
+
   fclose(fp);
 
 // 	int t;//计算CRC
